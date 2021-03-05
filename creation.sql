bb304ed0206bf961741511f743ba14225440ea76
Pratasenia Dzmitry <pzmicer@hotmail.com>
2021-03-05 23:11:22 +0300

    some changes

--- creation.sql
+++ creation.sql
@@ -1,5 +1,4 @@
-create table books
-(
+create table books (
 	id serial
 		constraint books_pk
 			primary key,
@@ -13,26 +12,17 @@ create table books
 	profit money
 );
 
-insert into books (book_name, author_name, author_surname, year, publisher, cost, price, profit)
-    values ('Harry Potter and the Philosopher''s Stone', 'Joan', 'Rowling', 1997, 'Bloomsbury', 15, 20, 1000);
-
-insert into books (book_name, author_name, author_surname, year, publisher, cost, price, profit)
-    values ('Witcher', 'Andrzej', 'Sapkowski', 1986, 'superNOWA', 10, 15, 1500);
-
-insert into books (book_name, author_name, author_surname, year, publisher, cost, price, profit)
-    values ('The Lord of the Rings', 'John', 'Tolkien', 1954, 'George Allen & Unwin', 25, 30, 1200);
-
-insert into books (book_name, author_name, author_surname, year, publisher, cost, price, profit)
-    values ('Ready Player One', 'Ernest', 'Cline', 2011, 'Random House', 18, 25, 800);
-
-insert into books (book_name, author_name, author_surname, year, publisher, cost, price, profit)
-    values ('Collection of problems on the theory of algorithms', 'Sergey', 'Sobol', 2020, 'Minsk BSU', 5, 8, 240);
+insert into books (book_name, author_name, author_surname, year, publisher, cost, price, profit) values
+    ('Harry Potter and the Philosopher''s Stone', 'Joan', 'Rowling', 1997, 'Bloomsbury', 15, 20, 1000),
+    ('Witcher', 'Andrzej', 'Sapkowski', 1986, 'superNOWA', 10, 15, 1500),
+    ('The Lord of the Rings', 'John', 'Tolkien', 1954, 'George Allen & Unwin', 25, 30, 1200),
+    ('Ready Player One', 'Ernest', 'Cline', 2011, 'Random House', 18, 25, 800),
+    ('Collection of problems on the theory of algorithms', 'Sergey', 'Sobol', 2020, 'Minsk BSU', 5, 8, 240);
 
 alter table books
     add type_id int;
 
-create table category
-(
+create table category (
     id serial
         constraint category_pk
             primary key,
@@ -40,14 +30,10 @@ create table category
     cat_description text
 );
 
-insert into category (cat_name, cat_description)
-    values ('Fantasy', '');
-
-insert into category (cat_name, cat_description)
-    values ('Studying', '');
-
-insert into category (cat_name, cat_description)
-    values ('Science fiction', '');
+insert into category (cat_name, cat_description) values
+    ('Fantasy', 'something'),
+    ('Studying', 'very'),
+    ('Science fiction', 'interesting');
 
 update books
     set type_id = 1
